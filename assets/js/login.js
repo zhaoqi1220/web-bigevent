95629ed2c38ef4377aa66f01fbce313af8816070
zhaoqi <542614239@qq.com>
2020-07-05 22:35:12 +0800

    完成了登录注册

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,6 +12,7 @@ $(function () {
 
     // 从layui中获取form对象
     var form = layui.form
+    var layer = layui.layer
     // // 通过form.verify()函数自定义校验规则
     form.verify({
         //     // 自定义一个叫做PWD校验规则
@@ -35,7 +36,61 @@ $(function () {
                 // 如果失败,return一个提示消息
                 return '两次密码不一致'
             }
+
+
         }
 
+
+
     })
+
+    // 监听注册表单事件
+
+    $('#form-reg').submit(function (e) {
+        // 阻止默认提交事件
+        e.preventDefault()
+        $.ajax({
+            url: '/api/reguser',
+            method: 'POST',
+            data: { username: $('#form-reg [name=username]').val(), password: $('#form-reg [name=username]').val() },
+
+            success: function (res) {
+                console.log(res)
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                return layer.msg(res.message)
+
+            }
+
+        })
+        // 模拟点击事件
+        $('#link_login').click()
+    })
+    // 监听登录表单事件
+    $('#form-login').submit(function (e) {
+        // 阻止默认提交事件
+        e.preventDefault()
+        // 发起AJAX请求
+        $.ajax({
+            url: '/api/login',
+            method: 'post',
+            data: { username: $('#form-login [name=username]').val(), password: $('#form-login [name=username]').val() },
+            success: function (res) {
+                if (res.status !== 0) {
+
+                    return layer.msg('登录失败')
+                }
+
+                layer.msg('登录成功')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中 
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+
+            }
+
+        })
+
+    })
+
 })
